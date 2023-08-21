f0e9f7878f2590f5379959f9b6f021ead29bebfd
Shahabr99 <sean.rohani9@gmail.com>
2023-08-20 23:41:22 -0700

    part1 completed

---
+++ part-1/app.js
@@ -0,0 +1,9 @@
+const App = () => (
+  <div>
+    <h1>My veryt first component</h1>
+    <Para name="Shahab" />
+  </div>
+  
+)
+
+ReactDOM.render(<App />, document.getElementById("root"))
---
+++ part-1/para.js
@@ -0,0 +1,7 @@
+const Para = (props) => {
+  return (
+    <div>
+      <p>My name is {props.name}</p>
+    </div>
+  )
+}
